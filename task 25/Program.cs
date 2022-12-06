160d089d29332dee7b1300d0dc3a99f7b7abe554
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-07 00:37:20 +0300

    task 25

---
+++ task 25/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число A - ");
+double x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B - ");
+double y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+double result = Math.Pow(x, y);
+Console.Write(result);
