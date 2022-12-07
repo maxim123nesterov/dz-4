e9c43d0e830bcfcb67b43445556825e183f0ab0b
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-08 01:19:07 +0300

    задание 27 провал

--- task 27/Program.cs
+++ task 27/Program.cs
@@ -1,23 +1,14 @@
-﻿Console.Write("Введите число трёх значное число - ");
+﻿Console.Write("Введите число - ");
 int a = Convert.ToInt32(Console.ReadLine());
-//int b = a % 10;
-int i = a % 10;
 int sum = 0;
-//int c = (a / 10) % 10;
+while (a > 0)
+{
+ a = a % 10;
+ sum = sum + a;
+    } 
+Console.WriteLine(sum);
+
+/*int c = (a / 10) % 10;
 //it d = a / 100;
 //int sum = b + c + d;
-
-for  (i = a % 10; i <= a; i ++)
-{
-    sum = sum + i;
-    
-    Console.WriteLine(sum);
-    return;
-}
-/*Console.WriteLine();
-Console.WriteLine(b);
-Console.WriteLine();
-Console.WriteLine(c);
-Console.WriteLine();
-Console.WriteLine(d);
 Console.Write($"Сумма цифр в числе = {sum}");*/
