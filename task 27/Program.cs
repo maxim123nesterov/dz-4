4d48f3516292cdc25eeaf3580c837b1fca7fef9c
Maxim <maxmaxmaxmaxmaxa@mail.ru>
2022-12-07 03:37:40 +0300

    начало

---
+++ task 27/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите число трёх значное число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+//int b = a % 10;
+int i = a % 10;
+int sum = 0;
+//int c = (a / 10) % 10;
+//it d = a / 100;
+//int sum = b + c + d;
+
+for  (i = a % 10; i <= a; i ++)
+{
+    sum = sum + i;
+    
+    Console.WriteLine(sum);
+    return;
+}
+/*Console.WriteLine();
+Console.WriteLine(b);
+Console.WriteLine();
+Console.WriteLine(c);
+Console.WriteLine();
+Console.WriteLine(d);
+Console.Write($"Сумма цифр в числе = {sum}");*/
